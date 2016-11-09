b5111a3386804e9db35f4921326ab14759f03e98
Tom Lee <contact@tomlee.com.au>
2016-11-09 14:34:42 +1000

    Add new functionality

--- src/AtomTrajectories.jl
+++ src/AtomTrajectories.jl
@@ -1,7 +1,8 @@
 module AtomTrajectories
 
 export Trajectory, Frame
-export gettime, settime!, getbox, setbox!, getnumatoms, getpositions
+export gettime, settime!, getbox, setbox!, setorigin!
+export getnumatoms, getpositions, shiftpositions!
 export getframe, advance!
 
 using SimulationBoxes
@@ -29,13 +30,27 @@ getnumatoms(f::Frame) = size(f.positions,1)
 Base.getindex(f::Frame, atomid::Integer) = f.positions[atomid]
 Base.setindex!{V}(f::Frame{V}, pos, atomid::Integer) = f.positions[atomid] = pos
 
-getpositions(f::Frame) = f.positions
+getpositions(frame::Frame) = frame.positions
+
+function shiftpositions!(frame::Frame, displacement)
+    pos = getpositions(frame)
+    for i in eachindex(pos)
+        pos[i] += displacement
+    end
+    frame
+end
 
 gettime(f::Frame) = f.time
 settime!(f::Frame, value) = f.time = value
 
 getbox(f::Frame) = f.box
 setbox!(f::Frame, box) = f.box = box
+
+function SimulationBoxes.wrap!(frame::Frame)
+    wrap!(getpositions(frame), getbox(frame))
+end
+
+setorigin!(frame::Frame, origin) = setbox!(frame, setorigin(getbox(frame), origin))
 
 
 end
